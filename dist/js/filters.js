0f391160670cdac8379498227b4d7d0c543bb2d3
mharyst <mykhailo.haryst@gmail.com>
2016-08-19 01:48:43 +0300

    Added dist folder

---
+++ dist/js/filters.js
@@ -0,0 +1,6 @@
+angular.module('forecastApp.filters', [])
+  .filter('placeholder', [function() {
+    return function (input,phvalue) {
+      return (angular.isUndefined(input) || input == '') ? phvalue : input;
+    };
+  }])
