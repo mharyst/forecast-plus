92963a646604882d8b04c90ebb0aa50c62a0791d
mharyst <mykhailo.haryst@gmail.com>
2016-10-25 02:55:59 +0300

    Delet dist folder & update index.html

--- dist/js/filters.js
+++
@@ -1,6 +0,0 @@
-angular.module('forecastApp.filters', [])
-  .filter('placeholder', [function() {
-    return function (input,phvalue) {
-      return (angular.isUndefined(input) || input == '') ? phvalue : input;
-    };
-  }])
